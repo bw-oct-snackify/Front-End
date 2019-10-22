fcf38d4a30fc2566131000767f8053b3ebe6c9aa
Arty <markyshuk@gmail.com>
2019-10-22 10:00:40 -0700

    Added more styling

--- src/components/Dashboard/Checkout/SnackView/SnackView.jsx
+++ src/components/Dashboard/Checkout/SnackView/SnackView.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./snackview.module.scss";
 import MySnack from "./MySnack/MySnack";
+import { axiosInstance } from "../../../../utils/axiosInstance";
 
 const SnackView = () => {
   const [snackList, setSnackList] = useState([
@@ -19,6 +20,20 @@ const SnackView = () => {
         "https://target.scene7.com/is/image/Target/GUEST_ac2b08b4-12e8-496c-ab09-dd530740da9c?wid=488&hei=488&fmt=pjpeg"
     }
   ]);
+  const companyCode = "lambda-school-snackify-123";
+
+  useEffect(() => {
+    let instance = axiosInstance();
+    instance
+      .get(`/company/${companyCode}/snacks`)
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>Snacks</h2>
